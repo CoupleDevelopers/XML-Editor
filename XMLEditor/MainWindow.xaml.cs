d9b8fa7f13b3c90904b964a96481844b12fdbef2
Ardahan-Kisbet <ardahankisbet@gmail.com>
2021-12-30 21:34:54 +0100

    initial commit

---
+++ XMLEditor/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace XMLEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DraggableWindow(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
