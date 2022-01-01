6c31ff67f73980e468ef3905f763e47d8764a5bc
Volkan DOĞAN <vlkndogan@gmail.com>
2022-01-01 23:17:00 +0300

    XElement Extension Methods

---
+++ src/XMLOperations/Extensions/XElementExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Linq;
+
+namespace XMLOperations.Extensions
+{
+    internal static class XElementExtensions
+    {
+
+        /// <summary>
+        /// Determines whether an XElement contains a specified attribute by <see cref="XName.LocalName"/>
+        /// <br />with <paramref name="withValue"/> if provided.
+        /// </summary>
+        public static bool ContainsAttribute(this XElement element, string attribute, string? withValue = null)
+        {
+            if (!element.HasAttributes) return false;
+
+            return element.Attributes().Any(x => x.Name.LocalName == attribute && (withValue == null || x.Value == withValue));
+        }
+
+
+        /// <summary>
+        /// Determines whether an XElement contains a specified attribute using <paramref name="predicate"/> function.
+        /// </summary>
+        public static bool ContainsAttribute(this XElement element, Func<XAttribute,bool> predicate)
+        {
+            if (!element.HasAttributes) return false;
+
+            return element.Attributes().Any(x => predicate(x));
+        }
+    }
+}
