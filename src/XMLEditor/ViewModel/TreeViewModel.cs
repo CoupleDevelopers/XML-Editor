ff2ac317a09e37267447392c1b088f560a051b63
Volkan DOĞAN <vlkndogan@gmail.com>
2022-01-09 11:30:18 +0300

    Update TreeViewModel.cs
    Load xml from url function added

--- src/XMLEditor/ViewModel/TreeViewModel.cs
+++ src/XMLEditor/ViewModel/TreeViewModel.cs
@@ -1,5 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using System.Xml.Linq;
 using XMLEditor.Extensions;
 using XMLEditor.Model;
@@ -15,8 +20,11 @@ namespace XMLEditor.ViewModel
         public TreeViewModel()
         {
 #warning Needs to be loaded with Load File button
-            var filePath = @"C:\Users\Volkan\Desktop\WorkSpace\Github\XML-Editor\test\XMLOperations.Tests\Assets\XMLFile1.xml";
-            LoadTreeFromFile(filePath);
+            //var filePath = @"C:\Users\Volkan\Desktop\WorkSpace\Github\XML-Editor\test\XMLOperations.Tests\Assets\XMLFile1.xml";
+            //LoadTreeFromFile(filePath);
+
+            var task = Task.Run(async () => { await LoadTreeFromUrl("https://services.odata.org/v3/northwind/northwind.svc/$metadata"); });
+            task.Wait();
         }
 
         private void LoadTreeFromFile(string filePath)
@@ -29,6 +37,19 @@ namespace XMLEditor.ViewModel
             var root = XDocument.Load(filePath).Root;
 
             RootNode = root.ToTreeNode();
+        }
+
+        private async Task LoadTreeFromUrl(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+
+            using (StreamReader streamReader = new StreamReader(await response.Content.ReadAsStreamAsync()))
+            {
+                string resultString = await streamReader.ReadToEndAsync();
+                var root = XDocument.Parse(resultString).Root;
+                RootNode = root.ToTreeNode();
+            }
         }
     }
 }
