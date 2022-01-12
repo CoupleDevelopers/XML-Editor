707d7dd29807ea62f00df3b916c53db76ac6a3c4
Volkan DOĞAN <vlkndogan@gmail.com>
2022-01-12 20:31:55 +0300

    TreeNode attributes added to treeview

--- src/XMLOperations/Types/TreeNode.cs
+++ src/XMLOperations/Types/TreeNode.cs
@@ -5,6 +5,7 @@ public class TreeNode
     public TreeNode(string name)
     {
         Name = name;
+        Attributes = new Dictionary<string, string>();
     }
 
     public string Name { get; set; }
@@ -13,5 +14,8 @@ public class TreeNode
     public Dictionary<string, string> Attributes { get; set; }
 
     public int ChildrenCount => Children.Count;
+
+    public string AttributesString => string.Join(" ", Attributes.Select(x => $"{x.Key}=\"{x.Value}\""));
+
     public List<TreeNode> Children { get; set; } = new List<TreeNode>();
 }
