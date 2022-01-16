cf855ec44a1658c6b0cf2bbf4b41a7db782f2087
Ardahan Kisbet <Ardahan-Kisbet@users.noreply.github.com>
2022-01-16 18:18:57 +0100

    Working on data bindings for tree comparison. Example structure is implemented.

--- src/XMLEditor/View/TreeView.xaml.cs
+++ src/XMLEditor/View/TreeView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace XMLEditor.View
 {
@@ -7,6 +9,15 @@ namespace XMLEditor.View
     /// </summary>
     public partial class TreeView : UserControl
     {
+        public string SourceLabel
+        {
+            get { return (string)GetValue(SourceLabelProperty); }
+            set { SetValue(SourceLabelProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceLabelProperty =
+            DependencyProperty.Register("SourceLabel", typeof(string), typeof(TreeView), new PropertyMetadata(string.Empty));
+
         public TreeView()
         {
             InitializeComponent();
--- src/XMLEditor/ViewModel/TreeComparisonViewModel.cs
+++ src/XMLEditor/ViewModel/TreeComparisonViewModel.cs
@@ -1,6 +1,31 @@
 ﻿namespace XMLEditor.ViewModel
 {
-    internal class TreeComparisonViewModel
+    internal class TreeComparisonViewModel : ViewModelBase
     {
+        private string m_LeftTreeName;
+        private string m_RightTreeName;
+
+        public string LeftTreeName
+        {
+            get => m_LeftTreeName;
+            set => SetField(ref m_LeftTreeName, value, () => LeftTreeName);
+        }
+
+        public string RightTreeName
+        {
+            get => m_RightTreeName;
+            set => SetField(ref m_RightTreeName, value, () => RightTreeName);
+        }
+
+        public TreeComparisonViewModel()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            m_LeftTreeName = "Tree 1";
+            m_RightTreeName = "Tree 2";
+        }
     }
 }
